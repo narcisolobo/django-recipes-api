a50f3dc0ea6701e4c2275f98479ffde691bba7f7
narcisolobo <narcisolobo@gmail.com>
2022-12-28 17:21:32 -0800

    drf-spectacular and swagger

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,7 +8,7 @@ from django.test import Client
 
 
 class AdminSiteTests(TestCase):
-    """ Tests for Django admin """
+    """ Tests for Django admin. """
 
     def setUp(self):
         """ Create user and client. """
